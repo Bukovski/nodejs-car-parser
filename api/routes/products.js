edee6521262bf9fd8a70d362598b31bd9ab4b002
Bukovski <kmanankov@bk.ru>
2021-01-28 15:34:52 +0300

    add: products update and delete

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,80 +4,81 @@ const mongoose = require('mongoose');
 const Product = require('../models/product')
 
 
-router.get('/', (req, res, next) => {
-	Product.find()
-		.exec()
-		.then(doc => {
-			console.log("GET products", doc)
-			
-			res.status(200).json(doc)
-		})
-		.catch(err => {
-			console.log(err)
-			
-			res.status(500).json({ error: err })
-		})
+router.get('/', async (req, res) => {
+	try {
+		const product = await Product.find({})
+		
+		res.status(200).json(product)
+	} catch (err) {
+		res.status(500).json({ error: err })
+	}
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
 	const product = new Product({
 		_id: new mongoose.Types.ObjectId(),
 		name: req.body.name,
 		price: req.body.price
 	});
 	
-	product
-		.save()
-		.then(result => {
-			console.log(result)
-			
-			res.status(200).json({
-				message: 'POST products',
-				createdProduct: result
-			})
+	try {
+		await product.save();
+		
+		res.status(200).json({
+			message: 'POST products',
+			createdProduct: product
 		})
-		.catch(err => {
-			console.log(err)
-			
-			res.status(500).json({
-				error: err
-			})
-		});
+	} catch (err) {
+		res.status(500).json({ error: err })
+	}
 })
 
-router.get("/:productId", (req, res, next) => {
-	const id = req.params.productId;
+router.get("/:productId", async (req, res, next) => {
+	const _id = req.params.productId;
 	
-	Product.findById(id)
-		.exec()
-		.then(doc => {
-			console.log("From DB", doc)
-			
-			if (doc) {
-				res.status(200).json(doc)
-			} else {
-				res.status(404).json({ message: 'No valid Product ID'})
-			}
-		})
-		.catch(err => {
-			console.log(err)
-			
-			res.status(500).json({ error: err })
-		})
+	try {
+		const product = await Product.findById(_id)
+		
+		if (!product) return res.status(404).json({ message: 'No valid Product ID'})
+		
+		res.status(200).json(product)
+	} catch (err) {
+		res.status(500).json({ error: err })
+	}
 });
 
-router.patch("/:productId", (req, res, next) => {
-	res.status(200).json({
-		message: 'Updated product!',
-	})
+router.patch("/:productId", async (req, res, next) => {
+	const _id = req.params.productId;
+	
+	const updates = Object.keys(req.body)
+	const allowedUpdates = [ 'name', 'price' ]
+	const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+	
+	if (!isValidOperation) return res.status(400).send({ error: 'Invalid fields for updates!' })
+
+	try {
+		const product = await Product.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+		if (!product) return res.status(404).json({ error: "Product Id not found" });
+		
+		res.status(200).json(product)
+	} catch (err) {
+		res.status(400).json({ error: err })
+	}
 });
 
-router.delete("/:productId", (req, res, next) => {
-	res.status(200).json({
-		message: 'Deleted product!',
-	})
+router.delete("/:productId", async (req, res, next) => {
+	const _id = req.params.productId;
+	
+	try {
+		const product = await Product.findByIdAndDelete(_id)
+		
+		if (!product) return res.status(404).json({ error: "Product Id not found" });
+		
+		res.status(200).json(product)
+	} catch (err) {
+		res.status(500).json({ error: err })
+	}
 });
-
 
 
 
